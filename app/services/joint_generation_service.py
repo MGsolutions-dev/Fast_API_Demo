a14b2059ede7af1f639c36826dcee8d7296cd7c7
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2023-01-24 13:36:18 +0900

    add asyncio example

--- app/services/joint_generation_service.py
+++ app/services/joint_generation_service.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import aiohttp
 import numpy as np
 
 from app.repositories.joint_model_repository import JointModelRepository
@@ -8,7 +11,17 @@ class JointGenerationService:
     def __init__(self, joint_model_repository: JointModelRepository):
         self.joint_model_repository = joint_model_repository
 
-    def generate(self, image_url: str) -> GeneratedJoints:
-        # TODO download image and convert to numpy array
+    async def generate(self, image_url: str) -> GeneratedJoints:
+        async with aiohttp.ClientSession() as session:
+            result1, result2, result3 = await asyncio.gather(
+                session.post(""),
+                session.post(""),
+                session.post(""),
+            )
+            generated_joint_1 = GeneratedJoints.parse_raw(**result1.json())
+            print(generated_joint_1)
+            print(result2.json())
+            print(result3.json())
+            # TODO download image and convert to numpy array
         image = np.array([0.0])
         return self.joint_model_repository.generate_joints(image)
